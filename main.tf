7ad0001960e80d9855ec8dc24d8ba60fce9a5794
Pranathi Alapati <tpranathi04@gmail.com>
2023-05-14 22:26:24 +0530

    adding env to ansible project

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_ssm_parameter" "parameter" {
   type  = "String"
   key_id = "df50dfa2-3f97-4213-b53f-deb991042c44"
 }
+
+
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value = var.passwords[count.index].value
+  type  = "SecureString"
+  key_id = "df50dfa2-3f97-4213-b53f-deb991042c44"
+}
